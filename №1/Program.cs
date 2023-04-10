f10958bd5262b3f022d2c2249b350e344d4fb214
Vladimir2077 <karlbernadot77@yandex.ru>
2023-04-10 15:54:55 +0200

    Added homework

---
+++ №1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+Console.WriteLine("Введите количество трехзначных чисел массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+for (int a = 0; a < numbers.Length; a++)
+if (numbers[a] % 2 == 0)
+count++;
+Console.WriteLine($"количество чисел {numbers.Length} , чётные {count} ");
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(099,999);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write(" ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write(" ");
+    Console.WriteLine();
+}
---
+++ №3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите количество чисел массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size]; 
+FillArrayRandomNumbers(numbers); 
+Console.WriteLine("Массив: ");
+PrintArray(numbers); 
+double min = Int32.MaxValue;
+double max = Int32.MinValue; 
+for (int a = 0; a < numbers.Length; a++) 
+{     
+    if (numbers[a] > max)        
+        { 
+        max = numbers[a];        
+        }     
+        if (numbers[a] < min)        
+        {
+            min = numbers[a];
+        } 
+}
+Console.WriteLine($"количество элементов {numbers.Length} , Максимальное число = {max} , Минимальное число = {min} ");
+Console.WriteLine($" разница между максимальным и минимальным числом = {max - min}"); 
+void FillArrayRandomNumbers(double[] numbers)
+    {
+        for(int i = 0; i < numbers.Length; i++)       
+            {
+                numbers[i] = Convert.ToDouble(new Random().Next(-1000,1000)) / 100; 
+            } 
+    } 
+    void PrintArray(double[] numbers)
+    {
+        Console.Write(" ");  
+        for(int i = 0; i < numbers.Length; i++)    
+            {
+            Console.Write(numbers[i] + " ");     
+            }
+            Console.Write(" ");
+            Console.WriteLine(); 
+    }
